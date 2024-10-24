369c26cc5fbe99f9b0b430bbd112b659722406b3
Jalzn <jalmirfsjr@gmail.com>
2024-10-24 20:59:40 -0300

    fix: fix wrong return in teacher repository interface

--- backend/src/domain/repositories/teacher.repository.ts
+++ backend/src/domain/repositories/teacher.repository.ts
@@ -1,7 +1,7 @@
 import { Teacher } from '../entities'
 
 export interface ITeacherRepository {
-    findAll: () => Promise<Teacher>
+    findAll: () => Promise<Teacher[]>
     findById: (id: string) => Promise<Teacher>
     create: (teacher: Teacher) => Promise<void>
     update: (teacher: Teacher, data: Partial<Teacher>) => Promise<void>
