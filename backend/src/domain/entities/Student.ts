38e5ace9cb123d4d5df1446323d050e038426c06
Jalzn <jalmirfsjr@gmail.com>
2024-11-02 17:29:00 -0300

    feat: add name field to student

--- backend/src/domain/entities/Student.ts
+++ backend/src/domain/entities/Student.ts
@@ -1,17 +1,21 @@
 type IStudentProps = {
     id: string
+    name: string
 }
 
 export default class Student {
     public id: string
+    public name: string
 
-    private constructor({ id }: IStudentProps) {
+    private constructor({ id, name }: IStudentProps) {
         this.id = id
+        this.name = name
     }
 
-    public static create() {
+    public static create(name: string) {
         return new Student({
             id: crypto.randomUUID().toString(),
+            name
         })
     }
 
